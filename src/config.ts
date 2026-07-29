aadd707112a1150f40b6643ab041ecb69a48d764
Tobias Bollinger <tobias.bollinger@bs.ch>
2026-07-29 09:31:59 +0200

    updated the redirect url to kdkp.intranet.bs.ch

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,11 @@
 export type AppConfig = {
     homepageUrl: string;
-    redirects: [
-        {
-            relativePath: string;
-            checkUrl: string;
-            to: string;
-            appName: string;
-        },
-    ];
+    redirects: {
+        relativePath: string;
+        checkUrl: string;
+        to: string;
+        appName: string;
+    }[];
 };
 
 export const config = {
@@ -15,27 +13,27 @@ export const config = {
     redirects: [
         {
             relativePath: "textmate",
-            to: "https://textmate.uvk8sbswc19.cmp.bs.ch",
-            checkUrl: "https://textmate.uvk8sbswc19.cmp.bs.ch/api/ping",
+            to: "https://textmate.kdkp.intranet.bs.ch",
+            checkUrl: "https://textmate.kdkp.intranet.bs.ch/api/ping",
             appName: "TextMate",
         },
         {
             relativePath: "transcribo",
-            to: "https://transcribo.uvk8sbswc19.cmp.bs.ch",
-            checkUrl: "https://transcribo.uvk8sbswc19.cmp.bs.ch/api/ping",
+            to: "https://transcribo.kdkp.intranet.bs.ch",
+            checkUrl: "https://transcribo.kdkp.intranet.bs.ch/api/ping",
             appName: "Transcribo",
         },
         {
             relativePath: "bs-uebersetzer",
-            to: "https://bs-uebersetzer.uvk8sbswc19.cmp.bs.ch/",
-            checkUrl: "https://bs-uebersetzer.uvk8sbswc19.cmp.bs.ch/api/ping",
+            to: "https://bs-uebersetzer.kdkp.intranet.bs.ch",
+            checkUrl: "https://bs-uebersetzer.kdkp.intranet.bs.ch/api/ping",
             appName: "BS Übersetzer",
         },
         {
             relativePath: "extern",
-            to: "https://extern.uvk8sbswc19.cmp.bs.ch/",
+            to: "https://extern.kdkp.intranet.bs.ch",
             checkUrl: "https://no-exist/api/ping",
             appName: "Extern Test",
         },
     ],
-};
+} satisfies AppConfig;
